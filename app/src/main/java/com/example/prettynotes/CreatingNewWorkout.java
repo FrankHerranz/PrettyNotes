2242aa5e10ce505d4ddd6027adc78df6e205e1d8
Frank <luifraher@gmail.com>
2019-09-01 12:33:47 +0200

    Finished logic of creating a new workout.

--- app/src/main/java/com/example/prettynotes/CreatingNewWorkout.java
+++ app/src/main/java/com/example/prettynotes/CreatingNewWorkout.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.CheckBox;
 
 public class CreatingNewWorkout extends AppCompatActivity {
 
@@ -18,6 +19,7 @@ public class CreatingNewWorkout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creating_new_workout);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("New Workout");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -25,9 +27,28 @@ public class CreatingNewWorkout extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //Check if title has been filled.
-                if(((TextInputLayout)findViewById(R.id.workout_title)).getEditText().getText().toString().isEmpty())
-                    Snackbar.make(view, "Entering a title is mandatory", Snackbar.LENGTH_LONG)
+            /*The workout can be saved if the following requisites are met:
+            It has a title  -- Done!
+            At least one of the days is checked -- TBD
+            There is at least one exercise -- TBD*/
+
+
+
+            if(((TextInputLayout)findViewById(R.id.workout_title)).getEditText().getText()
+                    .toString().isEmpty())
+                Snackbar.make(view, "Enter a title", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+            if (!(((CheckBox)findViewById(R.id.checkbox_monday)).isChecked()
+                    || ((CheckBox)findViewById(R.id.checkbox_tuesday)).isChecked()
+                    || ((CheckBox)findViewById(R.id.checkbox_wednesday)).isChecked()
+                    || ((CheckBox)findViewById(R.id.checkbox_thursday)).isChecked()
+                    || ((CheckBox)findViewById(R.id.checkbox_friday)).isChecked()
+                    || ((CheckBox)findViewById(R.id.checkbox_saturday)).isChecked()
+                    || ((CheckBox)findViewById(R.id.checkbox_sunday)).isChecked()
+
+            ))
+                Snackbar.make(view, "Check at least one day", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
